c5ba5f2ebeae2ff7fad28649f9b6441c2becc91c
Valeriu Craciun <valeriu.craciun@cognitive.ro>
2018-05-10 20:20:52 +0300

    Initial commit

---
+++ src/main/java/ro/cognitive/timeservice/TimeServiceApplication.java
@@ -0,0 +1,12 @@
+package ro.cognitive.timeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeServiceApplication.class, args);
+    }
+}

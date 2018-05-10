f60de04f81e575025b1ca129ae56b519db348a45
Valeriu Craciun <valeriu.craciun@cognitive.ro>
2018-05-10 23:11:12 +0300

    three new urls exposed /v1/getTime, /v2/getTime

--- src/main/java/ro/cognitive/timeservice/controller/TimeController.java
+++ src/main/java/ro/cognitive/timeservice/controller/TimeController.java
@@ -3,6 +3,7 @@ package ro.cognitive.timeservice.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ro.cognitive.timeservice.model.TimeModel;
+import ro.cognitive.timeservice.model.TimeModelV2;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -10,8 +11,14 @@ import java.util.concurrent.atomic.AtomicLong;
 public class TimeController {
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/getTime")
-    public TimeModel getTime() {
+    @RequestMapping("/v2/getTime")
+    public TimeModelV2 getTime2() {
+        return new TimeModelV2(counter.incrementAndGet());
+    }
+
+    @RequestMapping("/v1/getTime")
+    public TimeModel getTime1() {
         return new TimeModel(counter.incrementAndGet());
     }
+
 }

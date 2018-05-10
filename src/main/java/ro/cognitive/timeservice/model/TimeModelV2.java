4001940f566d8b1d7029c0b998335a7b05b4f39a
Valeriu Craciun <valeriu.craciun@cognitive.ro>
2018-05-10 23:06:20 +0300

    A new getTime model added

---
+++ src/main/java/ro/cognitive/timeservice/model/TimeModelV2.java
@@ -0,0 +1,25 @@
+package ro.cognitive.timeservice.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeModelV2 {
+    private final long id;
+    private String theTime;
+
+
+    public TimeModelV2(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTheTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        LocalDateTime now = LocalDateTime.now();
+        this.theTime = dtf.format(now);
+        return theTime;
+    }
+}
